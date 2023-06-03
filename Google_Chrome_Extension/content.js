0931fcac8d0246d514bc75ad174ab8f025df5801
itsuki-jp <itsuki19601@gmail.com>
2023-06-03 22:49:51 +0900

    change color by user input

--- Google_Chrome_Extension/content.js
+++ Google_Chrome_Extension/content.js
@@ -1,29 +1,24 @@
 console.log("AAAAAAAAAAAAAAAAA===========================")
 chrome.runtime.onMessage.addListener((request) => {
-    console.log("AAAAA");
-    if (request !== "change") return
-    changeColor();
+    console.log("A");
+    console.log(request);
+    console.log(request.type !== "change");
+    if (request.type !== "change") return;
+    changeColor(request);
     return true;
 })
 
 let tf = true;
 
-function changeColor() {
+function changeColor({ inputTag, inputColor }) {
+    console.log(inputColor, inputTag);
     const textCopy = document.getElementsByTagName("iframe")[0].contentDocument.getElementsByTagName("iframe")[0].contentDocument
     const body = textCopy.body;
-    body.getElementsByTagName("h1")[0].style.backgroundColor = "red";
-
-    tf = !tf;
-
-    const h1 = body.getElementsByTagName("h1");
-    for (let i = 0; i < h1.length; i++) {
-        changeElemBackColor(h1[i], tf ? "red" : "pink");
-    }
-
-    const h2 = body.getElementsByTagName("h2");
-    for (let i = 0; i < h2.length; i++) {
-        changeElemBackColor(h2[i], tf ? "pink" : "red");
-        changeElemColor(h2[i], tf ? "red" : "pink");
+    const tagElement = body.getElementsByTagName(inputTag);
+    console.log(tagElement);
+    for (let i = 0; i < tagElement.length; i++) {
+        console.log(i);
+        changeElemBackColor(tagElement[i], inputColor);
     }
 }
 
--- Google_Chrome_Extension/popup.js
+++ Google_Chrome_Extension/popup.js
@@ -1,10 +1,18 @@
 document.getElementById("clickButton").addEventListener("click", function () {
     console.log("クリック！");
+    const inputTag = document.getElementById("inputTag").value;
+    const inputColor = document.getElementById("inputColor").value;
+    if (inputTag === "" || inputColor === "") return;
     chrome.tabs.query({
         active: true,
         currentWindow: true
     }, function (tab) {
-        chrome.tabs.sendMessage(tab[0].id, 'change')
+        const obj = {
+            type: "change",
+            inputTag,
+            inputColor
+        }
+        chrome.tabs.sendMessage(tab[0].id, obj)
     })
 });
 
