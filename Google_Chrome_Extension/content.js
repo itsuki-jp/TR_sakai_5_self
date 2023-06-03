1c1f383a2300313667124906133b778d0f3de058
itsuki-jp <itsuki19601@gmail.com>
2023-06-03 22:03:04 +0900

    change color

--- Google_Chrome_Extension/content.js
+++ Google_Chrome_Extension/content.js
@@ -6,42 +6,31 @@ chrome.runtime.onMessage.addListener((request) => {
     return true;
 })
 
+let tf = true;
 
-function change() {
-    const h1Tags = document.getElementsByTagName("h1");
+function changeColor() {
+    const textCopy = document.getElementsByTagName("iframe")[0].contentDocument.getElementsByTagName("iframe")[0].contentDocument
+    const body = textCopy.body;
+    body.getElementsByTagName("h1")[0].style.backgroundColor = "red";
 
-    console.log("h1");
-    // Change the color of each h1 tag to pink
-    for (let i = 0; i < h1Tags.length; i++) {
-        console.log(h1Tags[i]);
-        h1Tags[i].style.color = "white";
-        h1Tags[i].style.backgroundColor = "pink";
+    tf = !tf;
+
+    const h1 = body.getElementsByTagName("h1");
+    for (let i = 0; i < h1.length; i++) {
+        changeElemBackColor(h1[i], tf ? "red" : "pink");
     }
 
-
-    const h2Tags = document.getElementsByTagName("p");
-    console.log("h2");
-    // Change the color of each h1 tag to pink
-    for (let i = 0; i < h2Tags.length; i++) {
-        console.log(h2Tags[i]);
-        h2Tags[i].style.color = "white";
-        h2Tags[i].style.backgroundColor = "green";
-    }   
+    const h2 = body.getElementsByTagName("h2");
+    for (let i = 0; i < h2.length; i++) {
+        changeElemBackColor(h2[i], tf ? "pink" : "red");
+        changeElemColor(h2[i], tf ? "red" : "pink");
+    }
 }
 
-document.getElementById("iframe").contentDocument.childNodes[1].childNodes[2].getElementsByTagName("h1")
+function changeElemColor(elem, color) {
+    elem.style.color = color;
+}
 
-document.getElementsByClassName("book")[0].childNodes[3].children[0].getElementsByTagName("h1")[0]
-
-textCopy = document.getElementsByTagName("iframe")[0].contentDocument.getElementsByTagName("iframe")[0].contentDocument.getElementsByTagName("body")[0].innerHTML
-// window.open().document.write(textCopy);
-
-textCopy = document.getElementsByTagName("iframe")[0].contentDocument.getElementsByTagName("iframe")[0].contentDocument
-// window.open().document.write(textCopy.head.innerHTML + textCopy.body.innerHTML);
-
-textCopy.body.getElementsByTagName("h1")[0].style.backgroundColor = "red"
-
-function changeColor(params) {
-    const textCopy = document.getElementsByTagName("iframe")[0].contentDocument.getElementsByTagName("iframe")[0].contentDocument
-    textCopy.body.getElementsByTagName("h1")[0].style.backgroundColor = "red"
+function changeElemBackColor(elem, color) {
+    elem.style.backgroundColor = color;
 }
