0def2010741eda43e4f9ed84d083b4d3114123a5
itsuki-jp <itsuki19601@gmail.com>
2023-06-10 23:37:37 +0900

    完成

--- Google_Chrome_Extension/content.js
+++ Google_Chrome_Extension/content.js
@@ -1,61 +1,60 @@
-// chrome.storage.sync.clear();
-// console.log("Data Clear!!!");
-
 chrome.runtime.onMessage.addListener((request) => {
     if (request.type === "log") {
         console.log(request.log);
         return;
     }
+    if (request.type === "clear") {
+        chrome.storage.sync.clear();
+        console.log("=====data clear=====");
+    }
     let storageData = null;
+    const { inputTag, inputStyleAtt, inputStyleVal } = request
     chrome.storage.sync.get(["changeColorExtension"]).then((result) => {
         console.log("resultBefore", result);
         storageData = result["changeColorExtension"] === undefined ? {} : result["changeColorExtension"];
         console.log("storageData", storageData);
 
         if (request.type === "change") {
-            console.log("Data: ", storageData);
             console.log("=====change=====")
+            console.log("Data: ", storageData);
             changeColor(storageData);
         }
         if (request.type === "add") {
             console.log("=====add=====");
             console.log("Data: ", storageData);
-            storageData[request.inputTag] = request.inputColor;
+            const tagObj = storageData[inputTag] === undefined ? {} : storageData[inputTag];
+            tagObj[inputStyleAtt] = inputStyleVal;
+            storageData[inputTag] = tagObj;
 
             chrome.storage.sync.set({
                 changeColorExtension: storageData
-            });
-
-            chrome.storage.sync.get(["changeColorExtension"]).then((result) => {
-                console.log("resultAfter", result);
             });
             changeColor(storageData);
         }
     });
     return true;
-})
-
-let tf = true;
+});
 
 function changeColor(data) {
     console.log("data", data);
-    const keys = Object.keys(data);
-    console.log("keys", keys);
+    const tags = Object.keys(data);
+    console.log("keys", tags);
     let textCopy = document.getElementsByTagName("iframe")[0].contentDocument.getElementsByTagName("iframe")[0];
     if (textCopy === undefined) {
-        textCopy = document.getElementsByTagName("iframe")[1].contentDocument.getElementsByTagName("iframe")[0].contentDocument;
-    } else {
-        textCopy = textCopy.contentDocument;
+        textCopy = document.getElementsByTagName("iframe")[1].contentDocument.getElementsByTagName("iframe")[0];
     }
+    textCopy = textCopy.contentDocument;
     const body = textCopy.body;
-    for (let i = 0; i < keys.length; i++){
-        const inputTag = keys[i];
-        console.log(inputTag);
-        const tagElement = body.getElementsByTagName(inputTag);
-        console.log("tagElement", tagElement);
-        for (let j = 0; j < tagElement.length; j++) {
-            console.log(tagElement[j]);
-            changeElemBackColor(tagElement[j], data[inputTag]);
+    for (let i = 0; i < tags.length; i++) {
+        const tag = tags[i];
+        console.log(tag);
+        const tagElements = body.getElementsByTagName(tag);
+        const tagData = data[tag];
+        for (let j = 0; j < tagElements.length; j++) {
+            const tagElement = tagElements[j];
+            for (att in tagData) {
+                changeElemStyle(tagElement, att, tagData[att]);
+            }
         }
     }
 }
@@ -66,4 +65,8 @@ function changeElemColor(elem, color) {
 
 function changeElemBackColor(elem, color) {
     elem.style.backgroundColor = color;
+}
+
+function changeElemStyle(elem, attribute, value) {
+    elem.style[attribute] = value;
 }
--- Google_Chrome_Extension/popup.js
+++ Google_Chrome_Extension/popup.js
@@ -7,16 +7,26 @@ document.getElementById("changeBtn").addEventListener("click", function () {
     sendContent(obj);
 });
 
+document.getElementById("clearBtn").addEventListener("click", () => { 
+    sendLog("clicked:clearBtn");
+    const obj = {
+        type: "clear",
+    }
+    sendContent(obj);
+});
+
 document.getElementById("addBtn").addEventListener("click", function () {
     sendLog("clicked:addBtn");
     const inputTag = document.getElementById("inputTag").value;
-    const inputColor = document.getElementById("inputColor").value;
-    if (inputTag === "" || inputColor === "") return;
+    const inputStyleAtt = document.getElementById("inputStyleAttribute").value;
+    const inputStyleVal = document.getElementById("inputStyleValue").value;
+    if (inputTag === "" || inputStyleAtt === "") return;
 
     const obj = {
         type: "add",
         inputTag,
-        inputColor
+        inputStyleVal,
+        inputStyleAtt
     };
     sendContent(obj);
 });
@@ -24,7 +34,34 @@ document.getElementById("addBtn").addEventListener("click", function () {
 document.getElementById("listVisibility").addEventListener("click", () => {
     const optionList = document.getElementById("optionList");
     optionList.hidden = !optionList.hidden;
-})
+    if (optionList.hidden === false) {
+        updateOptionList()
+    }
+});
+
+function updateOptionList() {
+    chrome.storage.sync.get(["changeColorExtension"]).then((result) => {
+        const storageData = result["changeColorExtension"] === undefined ? {} : result["changeColorExtension"];
+        const tags = Object.keys(storageData);
+        tags.sort();
+        const optionListElem = document.getElementById("optionList");
+        optionListElem.innerHTML = "";
+        for (let tagIdx = 0; tagIdx < tags.length; tagIdx++) {
+            const liTag = document.createElement("li");
+            liTag.innerText = tags[tagIdx];
+            optionListElem.appendChild(liTag);
+            const ulAtt = document.createElement("ul");
+            optionListElem.appendChild(ulAtt);
+            const att = Object.keys(storageData[tags[tagIdx]]);
+            att.sort();
+            for (let i = 0; i < att.length; i++){
+                const liAtt = document.createElement("li");
+                liAtt.innerText = `${att[i]}:${storageData[tags[tagIdx]][att[i]]}`;
+                ulAtt.appendChild(liAtt);
+            }
+        }
+    });
+}
 
 function sendContent(content) {
     chrome.tabs.query({
