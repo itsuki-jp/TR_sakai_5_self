ec9f44d6823beb3023abbf55027e8822c95a8b8e
itsuki-jp <itsuki19601@gmail.com>
2023-06-10 11:35:02 +0900

    DONE:任意のタグの背景色を変える

--- Google_Chrome_Extension/content.js
+++ Google_Chrome_Extension/content.js
@@ -1,24 +1,62 @@
-console.log("AAAAAAAAAAAAAAAAA===========================")
+// chrome.storage.sync.clear();
+// console.log("Data Clear!!!");
+
 chrome.runtime.onMessage.addListener((request) => {
-    console.log("A");
-    console.log(request);
-    console.log(request.type !== "change");
-    if (request.type !== "change") return;
-    changeColor(request);
+    if (request.type === "log") {
+        console.log(request.log);
+        return;
+    }
+    let storageData = null;
+    chrome.storage.sync.get(["changeColorExtension"]).then((result) => {
+        console.log("resultBefore", result);
+        storageData = result["changeColorExtension"] === undefined ? {} : result["changeColorExtension"];
+        console.log("storageData", storageData);
+
+        if (request.type === "change") {
+            console.log("Data: ", storageData);
+            console.log("=====change=====")
+            changeColor(storageData);
+        }
+        if (request.type === "add") {
+            console.log("=====add=====");
+            console.log("Data: ", storageData);
+            storageData[request.inputTag] = request.inputColor;
+
+            chrome.storage.sync.set({
+                changeColorExtension: storageData
+            });
+
+            chrome.storage.sync.get(["changeColorExtension"]).then((result) => {
+                console.log("resultAfter", result);
+            });
+            changeColor(storageData);
+        }
+    });
     return true;
 })
 
 let tf = true;
 
-function changeColor({ inputTag, inputColor }) {
-    console.log(inputColor, inputTag);
-    const textCopy = document.getElementsByTagName("iframe")[0].contentDocument.getElementsByTagName("iframe")[0].contentDocument
+function changeColor(data) {
+    console.log("data", data);
+    const keys = Object.keys(data);
+    console.log("keys", keys);
+    let textCopy = document.getElementsByTagName("iframe")[0].contentDocument.getElementsByTagName("iframe")[0];
+    if (textCopy === undefined) {
+        textCopy = document.getElementsByTagName("iframe")[1].contentDocument.getElementsByTagName("iframe")[0].contentDocument;
+    } else {
+        textCopy = textCopy.contentDocument;
+    }
     const body = textCopy.body;
-    const tagElement = body.getElementsByTagName(inputTag);
-    console.log(tagElement);
-    for (let i = 0; i < tagElement.length; i++) {
-        console.log(i);
-        changeElemBackColor(tagElement[i], inputColor);
+    for (let i = 0; i < keys.length; i++){
+        const inputTag = keys[i];
+        console.log(inputTag);
+        const tagElement = body.getElementsByTagName(inputTag);
+        console.log("tagElement", tagElement);
+        for (let j = 0; j < tagElement.length; j++) {
+            console.log(tagElement[j]);
+            changeElemBackColor(tagElement[j], data[inputTag]);
+        }
     }
 }
 
--- Google_Chrome_Extension/popup.js
+++ Google_Chrome_Extension/popup.js
@@ -1,19 +1,40 @@
-document.getElementById("clickButton").addEventListener("click", function () {
-    console.log("クリック！");
+// "Todo: https://*.nnn.ed.nico/courses/*" 以外のサイトの場合，alertを表示する
+document.getElementById("changeBtn").addEventListener("click", function () {
+    sendLog("clicked:changeBtn");
+    const obj = {
+        type: "change",
+    }
+    sendContent(obj);
+});
+
+document.getElementById("addBtn").addEventListener("click", function () {
+    sendLog("clicked:addBtn");
     const inputTag = document.getElementById("inputTag").value;
     const inputColor = document.getElementById("inputColor").value;
     if (inputTag === "" || inputColor === "") return;
+
+    const obj = {
+        type: "add",
+        inputTag,
+        inputColor
+    };
+    sendContent(obj);
+});
+
+document.getElementById("listVisibility").addEventListener("click", () => {
+    const optionList = document.getElementById("optionList");
+    optionList.hidden = !optionList.hidden;
+})
+
+function sendContent(content) {
     chrome.tabs.query({
         active: true,
         currentWindow: true
     }, function (tab) {
-        const obj = {
-            type: "change",
-            inputTag,
-            inputColor
-        }
-        chrome.tabs.sendMessage(tab[0].id, obj)
+        chrome.tabs.sendMessage(tab[0].id, content)
     })
-});
+}
 
-
+function sendLog(content) {
+    sendContent({ type: "log", log: content })
+}
